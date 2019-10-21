e5a80ccbeacce9f397937b6bf02467a67973c248
Troy Latchman <tlatchman@jacada.com>
2019-10-21 17:39:57 -0400

    Add comma to satisfy prettier rules

--- api/src/books/books.module.ts
+++ api/src/books/books.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 
 @Module({
-  controllers: [BooksController]
+  controllers: [BooksController],
 })
 export class BooksModule {}
