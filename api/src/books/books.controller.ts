415e8676145a7375a04f2d8a5ca8c1be202619ec
Troy Latchman <tlatchman@jacada.com>
2019-10-21 18:15:13 -0400

    Add books DTO and complete service/controller per Scotch IO tutorial: https://scotch.io/tutorials/getting-started-with-nestjs

--- api/src/books/books.controller.ts
+++ api/src/books/books.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Query,
+  Delete,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookID')
+  async getBook(@Param('bookID') bookID: string) {
+    const book = await this.booksService.getBook(bookID);
+    return book;
+  }
+
+  @Post()
+  async addBook(@Body() createBookDTO: CreateBookDTO) {
+    const book = await this.booksService.addBook(createBookDTO);
+    return book;
+  }
+
+  @Delete()
+  async deleteBook(@Query('bookID') bookID: string) {
+    const books = await this.booksService.deleteBook(bookID);
+    return books;
+  }
+}
--- api/src/books/books.service.ts
+++ api/src/books/books.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { BOOKS } from '../mocks/books.mock';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  books = BOOKS;
+
+  getBooks(): Promise<any> {
+    return new Promise(resolve => {
+      resolve(this.books);
+    });
+  }
+
+  getBook(bookID: string): Promise<any> {
+    const id = Number(bookID);
+    return new Promise(resolve => {
+      const book = this.books.find(currentBook => currentBook.id === id);
+      if (!book) {
+        throw new HttpException('Book does not exist!', 404);
+      }
+      resolve(book);
+    });
+  }
+
+  addBook(book: CreateBookDTO): Promise<any> {
+    return new Promise(resolve => {
+      this.books.push(book);
+      resolve(this.books);
+    });
+  }
+
+  deleteBook(bookID: string): Promise<any> {
+    const id = Number(bookID);
+    return new Promise(resolve => {
+      const index = this.books.findIndex(book => book.id === id);
+      if (index === -1) {
+        throw new HttpException('Book does not exist!', 404);
+      }
+      this.books.splice(index, 1);
+      resolve(this.books);
+    });
+  }
+}
