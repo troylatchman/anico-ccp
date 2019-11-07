9ee50ac7267b52a0fc7b8dea99ad34965fda33aa
Latchman <CT0871@dnanico1.aniconet.com>
2019-11-07 17:46:28 -0500

    Generate Module, Controller, and Service for Policy via Nest CLI.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { PolicyModule } from './policy/policy.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, PolicyModule],
   controllers: [AppController],
   providers: [AppService],
 })
