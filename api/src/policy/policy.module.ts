8d3e3363410ba35fee7d5eb75723ead0567663b2
Troy Latchman <tlatchman@jacada.com>
2019-11-11 19:59:18 -0500

    Move httpService config into Policy Module so the settings apply for all requests sent in Policy Service

--- api/src/policy/policy.module.ts
+++ api/src/policy/policy.module.ts
@@ -2,10 +2,24 @@ import { Module, HttpModule } from '@nestjs/common';
 import { PolicyController } from './policy.controller';
 import { PolicyService } from './policy.service';
 import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 
 @Module({
   controllers: [PolicyController],
   providers: [PolicyService],
-  imports: [HttpModule, ConfigModule],
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        headers: {
+          authorization: config.eccsAuthorizationHeader,
+          'x-ibm-client-id': config.eccsIbmClientId,
+        },
+        baseURL: config.eccsBaseUrl,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule,
+  ],
 })
 export class PolicyModule {}
--- api/src/policy/policy.service.ts
+++ api/src/policy/policy.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, HttpService, HttpException } from '@nestjs/common';
 import { PolicyLookupDTO } from './dto/policy-lookup.dto';
 import { ConfigService } from '../config/config.service';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class PolicyService {
@@ -9,26 +10,21 @@ export class PolicyService {
     private readonly httpService: HttpService,
   ) {}
 
+  handleAxiosError(error: AxiosError) {
+    if (error.response) {
+      throw new HttpException(error.response.data, error.response.status);
+    }
+    throw error;
+  }
+
   async billing(policyLookupDTO: PolicyLookupDTO) {
     try {
       const axiosResponse = await this.httpService
-        .post(
-          `${this.config.eccsBaseUrl}/billingapi/billinggetdata`,
-          policyLookupDTO,
-          {
-            headers: {
-              authorization: this.config.eccsAuthorizationHeader,
-              'x-ibm-client-id': this.config.eccsIbmClientId,
-            },
-          },
-        )
+        .post('/billingapi/billinggetdata', policyLookupDTO)
         .toPromise();
       return axiosResponse.data;
     } catch (error) {
-      if (error.response) {
-        throw new HttpException(error.response.data, error.response.status);
-      }
-      throw error;
+      this.handleAxiosError(error);
     }
   }
 }
