4b8f1a8c89c2f076ff613ca73ce187e25923a5ec
Troy Latchman <tlatchman@jacada.com>
2020-06-04 15:35:46 -0400

    Updates to be in line with ANCCP project.

--- client/src/services/BookService.js
+++ client/src/services/BookService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: "/ccp-api-local",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
